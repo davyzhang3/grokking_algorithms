180baf50e50a7d501cf57cf460777567e959654e
davyzhang3 <davyzhang325@gmail.com>
2021-07-26 11:10:31 -0400

    Chapter1

--- 01_introduction_to_algorithms/python/binary_search.py
+++ 01_introduction_to_algorithms/python/binary_search.py
@@ -1,51 +1,76 @@
 class BinarySearch():
 
-  def search_iterative(self, list, item):
-    # low and high keep track of which part of the list you'll search in.
-    low = 0
-    high = len(list) - 1
+  # def search_iterative(self, list, item):
+  #   # low and high keep track of which part of the list you'll search in.
+  #   low = 0
+  #   high = len(list) - 1
 
-    # While you haven't narrowed it down to one element ...
-    while low <= high:
-      # ... check the middle element
-      mid = (low + high) // 2
-      guess = list[mid]
-      # Found the item.
+  #   # While you haven't narrowed it down to one element ...
+  #   while low <= high:
+  #     # ... check the middle element
+  #     mid = (low + high) // 2
+  #     guess = list[mid]
+  #     # Found the item.
+  #     if guess == item:
+  #       return mid
+  #     # The guess was too high.
+  #     if guess > item:
+  #       high = mid - 1
+  #     # The guess was too low.
+  #     else:
+  #       low = mid + 1
+
+  #   # Item doesn't exist
+  #   return None
+  def search_iterative(self, lists, item):
+    high = len(lists)-1
+    low = 0
+    while high >= low:
+      mid = (high + low ) // 2
+      guess = lists[mid]
       if guess == item:
         return mid
-      # The guess was too high.
-      if guess > item:
+      elif guess > item:
         high = mid - 1
-      # The guess was too low.
       else:
-        low = mid + 1
-
-    # Item doesn't exist
+        low = mid +1
     return None
 
+  # def search_recursive(self, list, low, high, item):
+  #   # Check base case 
+  #   if high >= low: 
+  
+  #       mid = (high + low) // 2
+  #       guess = list[mid]
+  
+  #       # If element is present at the middle itself 
+  #       if guess == item:
+  #           return mid 
+  
+  #       # If element is smaller than mid, then it can only 
+  #       # be present in left subarray 
+  #       elif guess > item: 
+  #           return self.search_recursive(list, low, mid - 1, item) 
+  
+  #       # Else the element can only be present in right subarray 
+  #       else: 
+  #           return self.search_recursive(list, mid + 1, high, item) 
+  
+  #   else: 
+  #       # Element is not present in the array 
+  #       return None
   def search_recursive(self, list, low, high, item):
-    # Check base case 
-    if high >= low: 
-  
-        mid = (high + low) // 2
-        guess = list[mid]
-  
-        # If element is present at the middle itself 
-        if guess == item:
-            return mid 
-  
-        # If element is smaller than mid, then it can only 
-        # be present in left subarray 
-        elif guess > item: 
-            return self.search_recursive(list, low, mid - 1, item) 
-  
-        # Else the element can only be present in right subarray 
-        else: 
-            return self.search_recursive(list, mid + 1, high, item) 
-  
-    else: 
-        # Element is not present in the array 
-        return None
+    if low <= high:
+      mid = (low + high) // 2
+      guess = list[mid]
+      if guess == item:
+        return mid
+      elif guess > item:
+        return self.search_recursive(list, low, mid-1, item)
+      else:
+        return self.search_recursive(list, mid+1, high, item)
+    else:
+      return None
 
 if __name__ == "__main__":
   # We must initialize the class to use the methods of this class
